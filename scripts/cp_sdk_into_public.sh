f7cbc4d0a5caf12ac434dfcc2691568432d7e979
ilmoi <iljamoi@protonmail.com>
2023-05-25 16:47:00 -0400

    fix cp script:

--- scripts/cp_sdk_into_public.sh
+++ scripts/cp_sdk_into_public.sh
@@ -13,6 +13,7 @@ cp tsconfig.json ../tensorswap-sdk/tsconfig.json
 # fix the new package json
 sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ../tensorswap-sdk/yarn.lock
 sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ../tensorswap-sdk/package.json
+sed -i '' -e 's/@tensor-hq\/tensorswap-sdk/@tensor-oss\/tensorswap-sdk/g' ../tensorswap-sdk/package.json
 sed -i '' -e 's/tensorswap.git/tensorswap-sdk.git/g' ../tensorswap-sdk/package.json
 sed -i '' -e 's/https:\/\/npm.pkg.github.com/https:\/\/registry.npmjs.org/g' ../tensorswap-sdk/package.json
 sed -i '' -e 's/pubpush/npmpublish/g' ../tensorswap-sdk/package.json
