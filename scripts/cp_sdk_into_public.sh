7953ffd7748abff6f51a52ee6f5b4590e506b973
Richard Wu <richardwu1997@gmail.com>
2023-08-23 09:44:16 -0400

    Updated sdk cp script.

--- scripts/cp_sdk_into_public.sh
+++ scripts/cp_sdk_into_public.sh
@@ -1,25 +1,27 @@
+dest=../tensorswap-sdk
+
 # delete old
-rm -rf ../tensorswap-sdk/src
-rm ../tensorswap-sdk/package.json
-rm ../tensorswap-sdk/yarn.lock
-rm ../tensorswap-sdk/tsconfig.json
+rm -rf ${dest}/src
+rm ${dest}/package.json
+rm ${dest}/yarn.lock
+rm ${dest}/tsconfig.json
 
 # add new
-cp -r ./src ../tensorswap-sdk/src
-cp package.json ../tensorswap-sdk/package.json
-cp yarn.lock ../tensorswap-sdk/yarn.lock
-cp tsconfig.json ../tensorswap-sdk/tsconfig.json
+cp -r ./src ${dest}/src
+cp package.json ${dest}/package.json
+cp yarn.lock ${dest}/yarn.lock
+cp tsconfig.json ${dest}/tsconfig.json
 
 # fix the new package json
-sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ../tensorswap-sdk/yarn.lock
-sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/@tensor-hq\/tensorswap-sdk/@tensor-oss\/tensorswap-sdk/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/tensorswap.git/tensorswap-sdk.git/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/https:\/\/npm.pkg.github.com/https:\/\/registry.npmjs.org/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/pubpush/publish:public/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/yarn npm publish && yarn push/npm publish --access public --registry https:\/\/registry.npmjs.org/g' ../tensorswap-sdk/package.json
-sed -i '' -e 's/ && yarn tsc --p tsconfig.tests.json//g' ../tensorswap-sdk/package.json
+sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ${dest}/yarn.lock
+sed -i '' -e 's/tensorswap-ts/tensorswap-sdk/g' ${dest}/package.json
+sed -i '' -e 's/@tensor-hq\/tensorswap-sdk/@tensor-oss\/tensorswap-sdk/g' ${dest}/package.json
+sed -i '' -e 's/tensorswap.git/tensorswap-sdk.git/g' ${dest}/package.json
+sed -i '' -e 's/https:\/\/npm.pkg.github.com/https:\/\/registry.npmjs.org/g' ${dest}/package.json
+sed -i '' -e 's/pubpush/publish:public/g' ${dest}/package.json
+sed -i '' -e 's/yarn npm publish && yarn push/npm publish --access public --registry https:\/\/registry.npmjs.org/g' ${dest}/package.json
+sed -i '' -e 's/ && yarn tsc --p tsconfig.tests.json//g' ${dest}/package.json
 
-pushd ../tensorswap-sdk
+pushd ${dest}
 yarn
 popd
