2677d9871ae4e5df8dcbf7d49b806965526d0565
Richard Wu <richardwu1997@gmail.com>
2023-11-12 13:36:20 -0500

    v1.8.2: add margin withdraw CPI for TLOCK. (#52)
    * v1.8.2: add margin withdraw CPI for TLOCK.
    
    * Fix brick test errors.

--- programs/brick/src/lib.rs
+++ programs/brick/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::result_large_err)] // Needed otherwise clippy unhappy w/ anchor errors
 //! A brick.
 //! Deploy in case of emergency.
 
@@ -10,7 +11,7 @@ declare_id!("TSWAPaqyCSx2KABk68Shruf4rp7CxcNi8hAsbdwmHbN");
 pub mod brick {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
--- programs/tensorswap/src/instructions/margin/withdraw_margin_account.rs
+++ programs/tensorswap/src/instructions/margin/withdraw_margin_account.rs
@@ -125,7 +125,7 @@ pub struct WithdrawMarginAccountCpiTcomp<'info> {
     )]
     pub margin_account: Box<Account<'info, MarginAccount>>,
 
-    // this bid state can only be derived from TCOMP program for a given mint and owner
+    // this bid state can only be derived from TCOMP program for a given owner
     // and because it's a signer only TCOMP can call this
     // Don't want to import tcomp package just because of the key, so hardcoding
     #[account(mut,
@@ -138,13 +138,58 @@ pub struct WithdrawMarginAccountCpiTcomp<'info> {
     /// CHECK: has_one on margin_account, seeds in bid_state
     pub owner: UncheckedAccount<'info>,
 
-    /// CHECK: can only be passed in by TBID, since it has to sign off with bid pda
+    /// CHECK: can only be passed in by TCOMP, since it has to sign off with bid pda
     pub destination: UncheckedAccount<'info>,
 
     pub system_program: Program<'info, System>,
 }
 
 pub fn handler_cpi_tcomp(ctx: Context<WithdrawMarginAccountCpiTcomp>, lamports: u64) -> Result<()> {
+    transfer_lamports_from_pda(
+        &ctx.accounts.margin_account.to_account_info(),
+        &ctx.accounts.destination.to_account_info(),
+        lamports,
+    )
+}
+
+// --------------------------------------- cpi (tlock)
+
+#[derive(Accounts)]
+#[instruction(bump: u8, order_id: [u8; 32])]
+pub struct WithdrawMarginAccountCpiTLock<'info> {
+    #[account(
+        mut,
+        seeds = [
+            b"margin".as_ref(),
+            get_tswap_addr().as_ref(),
+            owner.key().as_ref(),
+            &margin_account.nr.to_le_bytes()
+        ],
+        bump = margin_account.bump[0],
+        has_one = owner,
+    )]
+    pub margin_account: Box<Account<'info, MarginAccount>>,
+
+    // this order state can only be derived from TLOCK program for a given owner
+    // and because it's a signer only TLOCK can call this
+    // Don't want to import tlock package just because of the key, so hardcoding
+    #[account(mut,
+        seeds=[b"order_state".as_ref(), owner.key().as_ref(), order_id.as_ref()],
+        seeds::program = Pubkey::from_str("TLoCKic2wGJm7VhZKumih4Lc35fUhYqVMgA4j389Buk").unwrap(),
+        bump = bump,
+    )]
+    pub order_state: Signer<'info>,
+
+    /// CHECK: has_one on margin_account, seeds in order_state
+    pub owner: UncheckedAccount<'info>,
+
+    /// CHECK: can only be passed in by TLOCK, since it has to sign off with order pda
+    pub destination: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler_cpi_tlock(ctx: Context<WithdrawMarginAccountCpiTLock>, lamports: u64) -> Result<()> {
     transfer_lamports_from_pda(
         &ctx.accounts.margin_account.to_account_info(),
         &ctx.accounts.destination.to_account_info(),
