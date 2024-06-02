8bff02a8cd8d1190e5a2955c4fbe655bc5948313
leantOnSol <leantOnSol@proton.me>
2024-06-02 22:04:40 +0200

    changed description of privkeyBytes

--- examples/common.ts
+++ examples/common.ts
@@ -1,10 +1,8 @@
 import { getBase58Encoder, createSolanaRpc } from "@solana/web3.js";
 
-// Uint8array with length of 64:
-// First 32 bytes == private key
-// Last 32 bytes == public key
+// Your private key as a base58 string as input
 export const keypairBytes = new Uint8Array(getBase58Encoder().encode("YOUR_PRIV_KEY_AS_BASE58_STRING"));
-// or alternatively directly as array of 64 bytes like this:
+// or alternatively directly as an array of 64 bytes like this:
 // export const keypairBytes = new Uint8Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
 export const helius_url = "https://mainnet.helius-rpc.com/?api-key=YOUR_HELIUS_RPC_KEY";
 export const rpc = createSolanaRpc(helius_url);
