af32796ca067500022ef779c818cedebc369daf0
Samuel Vanderwaal <samv@tensor.so>
2024-10-22 12:39:55 -0800

    use anchor explicitly in idl generation

--- scripts/generate-idls.mjs
+++ scripts/generate-idls.mjs
@@ -1,22 +1,24 @@
 #!/usr/bin/env zx
-import 'zx/globals';
-import { generateIdl } from '@metaplex-foundation/shank-js';
-import { getCargo, getProgramFolders } from './utils.mjs';
+import { generateIdl } from "@metaplex-foundation/shank-js";
+import "zx/globals";
+import { getCargo, getProgramFolders } from "./utils.mjs";
 
-const binaryInstallDir = path.join(__dirname, '..', '.cargo');
+const binaryInstallDir = path.join(__dirname, "..", ".cargo");
 
 getProgramFolders().forEach((folder) => {
   const cargo = getCargo(folder);
-  const isShank = Object.keys(cargo.dependencies).includes('shank');
-  const programDir = path.join(__dirname, '..', folder);
+  const programDir = path.join(__dirname, "..", folder);
 
   generateIdl({
-    generator: isShank ? 'shank' : 'anchor',
-    programName: cargo.package.name.replace(/-/g, '_'),
-    programId: cargo.package.metadata.solana['program-id'],
+    generator: "anchor",
+    programName: cargo.package.name.replace(/-/g, "_"),
+    programId: cargo.package.metadata.solana["program-id"],
     idlDir: programDir,
-    idlName: 'idl',
+    idlName: "idl",
     programDir,
     binaryInstallDir,
+    rustbin: {
+      locked: true
+    }
   });
 });
